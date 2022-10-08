46074796bda48c1f21c533a8da3f9416588c469a
Sayantan <sayantan.dps@gmail.com>
2022-10-08 14:33:12 +0530

    Commit

--- server/src/controllers/email.controller.js
+++ server/src/controllers/email.controller.js
@@ -3,55 +3,26 @@ const config = require("../config/config");
 const sgMail = require("@sendgrid/mail");
 const { emailTemplate } = require("../utils/emailTemplate");
 
-// sgMail.setApiKey(config.sendgrid.apiKey);
+sgMail.setApiKey(config.sendgrid.apiKey);
 
-// const sendEmail = async (req, res, next) => {
-//   const msg = {
-//     to: "sayantan.dps@gmail.com",
-//     from: "hackandsnack@sayantanmondal.com",
-//     subject: `New message "`,
-//     text: "Hello",
-//     html: emailTemplate(),
-//   };
+const sendEmail = async (req, res, next) => {
+  const msg = {
+    to: "sayantan.dps@gmail.com",
+    from: "naveen.209303050@muj.manipal.edu",
+    subject: `New message "`,
+    text: "Hello",
+    html: emailTemplate(),
+  };
 
-//   try {
-//     await sgMail.send(msg);
-//     res.status(httpStatus.OK).send("Email sent");
-//   } catch (error) {
-//     console.log(error);
-//     if (error.response) {
-//       console.error(error.response.body);
-//     }
-//     next(error);
-//   }
-// };
-
-const nodemailer = require("nodemailer");
-const sendEmail = async (items) => {
   try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.zoho.in",
-      port: 465,
-      secure: true,
-      auth: {
-        user: config.mailCreds.user,
-        pass: config.mailCreds.pass,
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: "dev@sayantanmondal.com",
-      to: "sayantan.dps@gmail.com",
-      subject: "Your Updates",
-      text: "Hello",
-      html: emailTemplate(items),
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    await sgMail.send(msg);
     res.status(httpStatus.OK).send("Email sent");
   } catch (error) {
     console.log(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+    next(error);
   }
 };
 
--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,54 +1,26 @@
 const { emailTemplate } = require("./emailTemplate");
-const nodemailer = require("nodemailer");
 const config = require("../config/config");
 const sgMail = require("@sendgrid/mail");
 
-// sgMail.setApiKey(config.sendgrid.apiKey);
-
-// const sendEmail = async (items) => {
-//   const msg = {
-//     to: "sayantan.dps@gmail.com",
-//     from: "dev@sayantanmondal.com",
-//     subject: `Your Updates"`,
-//     text: "Your Updates",
-//     html: emailTemplate(items),
-//   };
-
-//   try {
-//     await sgMail.send(msg);
-//     console.log("Mail Sent")
-//   } catch (error) {
-//     console.log(error);
-//     if (error.response) {
-//       console.error(error.response.body);
-//     }
-//   }
-// };
+sgMail.setApiKey(config.sendgrid.apiKey);
 
 const sendEmail = async (items) => {
+  const msg = {
+    to: "sayantan.dps@gmail.com",
+    from: "naveen.209303050@muj.manipal.edu",
+    subject: `Your Updates"`,
+    text: "Your Updates",
+    html: emailTemplate(items),
+  };
+
   try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.zoho.in",
-      port: 465,
-      secure: true,
-      auth: {
-        user: config.mailCreds.user,
-        pass: config.mailCreds.pass,
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: "dev@sayantanmondal.com",
-      to: "sayantan.dps@gmail.com",
-      subject: "Your Updates",
-      text: "Updates",
-      html: emailTemplate(items),
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    await sgMail.send(msg);
+    console.log("Mail Sent");
   } catch (error) {
     console.log(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
   }
 };
 
