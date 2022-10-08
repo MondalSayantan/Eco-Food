7ea8d261d787def9dc144c7c9e0ef6c8c05e70b0
Ambika Malhotra <ambikamalhotra@Ambikas-MacBook-Air.local>
2022-10-08 11:40:58 +0530

    Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header";
+import Addingredients from "./pages/addingredients";
 
 const App = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <Addingredients />
     </div>
   );
 };
